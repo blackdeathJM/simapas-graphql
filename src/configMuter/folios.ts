3e25f10027a68bc514033602de229054ffdc8c51
BLACKDEATH <blackdeath166@gmail.com>
2020-01-17 06:01:11 -0600

    Subir folios

--- src/configMuter/folios.ts
+++ src/configMuter/folios.ts
@@ -19,19 +19,17 @@ const storage = multer.diskStorage({
 const uploadArchivo = multer({
     storage,
     limits: {fileSize: 10000000},
-    fileFilter: (req, file, cb) =>
-    {
+    fileFilter: (req, file, cb) => {
         const filetypes = /jpeg|jpg|png|pdf/;
         const mimetype = filetypes.test(file.mimetype);
         const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
 
-        if (mimetype && extname)
-        {
+        if (mimetype && extname) {
             return cb(null, true);
         }
         cb(new Error('La imagen no es soportada'), false);
     }
-}).single('file');
+}).array('file');
 
 router.post('/upload/folios', function (req, res, next)
 {
