0c6faa932c8866981b12cbe4bedd15ecfe4e3e7b
BLACKDEATH <blackdeath166@gmail.com>
2020-01-21 08:00:48 -0600

    Actualizar el campo visto

---
+++ src/operaciones/querys/docInterna.ts
@@ -0,0 +1,14 @@
+export async function todasNotificacionesDocInterna(db: any) {
+    return await db.collection("docInterna").find().toArray();
+}
+
+export async function todasNotificacionesUsuario(usuario: string, db: any) {
+    console.log('Usuario', usuario);
+    return await db.collection("docInterna").find({"usuarioDestino.usuario": usuario}).toArray();
+}
+
+export async function docInternaUsuarioVisto(usuario: string, visto: boolean, db: any) {
+    // return await db.collection("docInterna").find({$and: [{"usuarioDestino.visto": visto, "usuarioDestino.usuario": usuario}]}).toArray();
+    // return await db.collection("docInterna").find({"usuarioDestino": {$elemMatch: {usuario, visto}}}).toArray();
+    return await db.collection("docInterna").find({"usuarioDestino": {$elemMatch: {usuario, visto}}}).toArray();
+}
--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -13,66 +13,68 @@ import {
     docsUsuarioEstatusPAR,
     todosLosDocumentos
 } from "../operaciones/querys/segDocumentacion.query";
+import {
+    docInternaUsuarioVisto,
+    todasNotificacionesDocInterna,
+    todasNotificacionesUsuario
+} from "../operaciones/querys/docInterna";
 
 const query: IResolvers =
     {
         Query:
             {
                 // DEPARTAMENTOS
-                async obtenerDepartamentos(_: void, __: any, {db})
-                {
+                async obtenerDepartamentos(_: void, __: any, {db}) {
                     return await obtenerDeptos(db)
                 },
-                async departamentoID(_: void, {_id}: any, {db})
-                {
+                async departamentoID(_: void, {_id}: any, {db}) {
                     return await buscarDeptoID(_id, db);
                 },
                 // USUARIOS
-                async obtenerUsuarios(_: void, __: any, {db})
-                {
+                async obtenerUsuarios(_: void, __: any, {db}) {
                     return await obtenerTodosLosUsuarios(db);
                 },
-                async buscarUsuario(_: any, {usuario}, {db})
-                {
+                async buscarUsuario(_: any, {usuario}, {db}) {
                     return await obtenerUsuarioPorSuNombreDeUsuario(usuario, db);
                 },
-                async login(_: void, {usuario, contrasena}, {db})
-                {
+                async login(_: void, {usuario, contrasena}, {db}) {
                     return await loginUsuario(usuario, contrasena, db)
                 },
-                async perfil(_: void, __: any, {token})
-                {
+                async perfil(_: void, __: any, {token}) {
                     return await perfilUsuario(token);
                 },
                 // FOLIOS
-                async obtenerFoliosTodos(_: void, __: void, {db})
-                {
+                async obtenerFoliosTodos(_: void, __: void, {db}) {
                     return await todosLosFolios(db);
                 },
-                async ultimoFolio(_: any, __: any, {db})
-                {
+                async ultimoFolio(_: any, __: any, {db}) {
                     return await folioUltimo(db);
                 },
-                async folioUsuario(_: void, {asigUsuario}, {db})
-                {
+                async folioUsuario(_: void, {asigUsuario}, {db}) {
                     return await folioPorUsuario(asigUsuario, db);
                 },
-                async todosDocumentos(_: void, __: void, {db})
-                {
+                async todosDocumentos(_: void, __: void, {db}) {
                     return await todosLosDocumentos(db);
                 },
                 // SEGUIMIENTO DE DOCUMENTACION
-                async obtenerDocsUsuario(_: void, {dirigido}, {db})
-                {
+                async obtenerDocsUsuario(_: void, {dirigido}, {db}) {
                     return await docsPorUsuario(dirigido, db);
                 },
-                async obtenerDocsUsuarioStatus(_: void, {dirigido, estatus, autorizado}, {db})
-                {
+                async obtenerDocsUsuarioStatus(_: void, {dirigido, estatus, autorizado}, {db}) {
                     return await docsPorUsuarioYEstatus(dirigido, estatus, autorizado, db);
                 },
-                async obtenerDocsUsuarioStatusPAR(_: void, {dirigido}, {db})
-                {
+                async obtenerDocsUsuarioStatusPAR(_: void, {dirigido}, {db}) {
                     return await docsUsuarioEstatusPAR(dirigido, db);
+                },
+                // DOCUMENTACION INTERNA
+                async obNotiDocInterna(_: void, __: void, {db}) {
+                    return await todasNotificacionesDocInterna(db);
+                },
+                async obNotiUsuario(_: void, {usuario}, {db}) {
+                    return await todasNotificacionesUsuario(usuario, db);
+                },
+                async obNotificacionesPorUsuarioYVisto(_: void, {usuario, visto}, {db}) {
+                    return await docInternaUsuarioVisto(usuario, visto, db);
                 }
             },
     }
