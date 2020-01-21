68b5b981f35b3cc6014468e5a59a62c619ca6f71
BLACKDEATH <blackdeath166@gmail.com>
2020-01-21 03:05:11 -0600

    Actualizar el campo visto

--- src/operaciones/mutations/docInterna.mutation.ts
+++ src/operaciones/mutations/docInterna.mutation.ts
@@ -1,5 +1,4 @@
-export async function agDocInterna(notificaion: any, db: any)
-{
+export async function agDocInterna(notificaion: any, db: any) {
     let totalNotificaciones = await db.collection("docInterna").countDocuments();
     if (totalNotificaciones != null) {
         totalNotificaciones += 1;
@@ -9,8 +8,7 @@ export async function agDocInterna(notificaion: any, db: any)
         notificaion.folioInterno = `FOL-${notificaion.numNotificacion}-SIMAPAS/${anoActual}`;
 
         return await db.collection("docInterna").insertOne(notificaion).then(
-            async () =>
-            {
+            async () => {
                 return {
                     estatus: true,
                     mensaje: 'Datos agregados con exito',
@@ -18,8 +16,7 @@ export async function agDocInterna(notificaion: any, db: any)
                 }
             }
         ).catch(
-            async (error: any) =>
-            {
+            async (error: any) => {
                 return {
                     estatus: false,
                     mensaje: 'Error en el servidor al intentar agregar la notificacion', error,
@@ -30,12 +27,16 @@ export async function agDocInterna(notificaion: any, db: any)
     }
 }
 
-export async function acVistoPorUsuario(usuario: string, folioInterno: string, db: any)
-{
-    return await db.collection("docInterna").findOneAndUpdate({$and: [{folioInterno}, {usuarioDestino: {usuario}}]}, {$set: {usuarioDestino: {visto: true}}}).then(
-        async (res: any) =>
-        {
-            console.log('ResultadoACT', res);
+export async function acVistoPorUsuario(usuario: string, folioInterno: string, db: any) {
+    return await db.collection("docInterna").findOneAndUpdate({$and: [{folioInterno}, {"usuarioDestino.usuario": usuario}]}, {$set: {"usuarioDestino.$.visto": true}},
+        false, true).then(
+        async (res: any) => {
+            console.log('Notificacion vista', res);
+            return {
+                estatus: true,
+                mensaje: 'La notificacion ha modificado como vista',
+                docInterna: res.value
+            }
         }
     ).catch();
 }
