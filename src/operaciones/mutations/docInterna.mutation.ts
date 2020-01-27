858a130fb527ad1638b4db973c0b5af7ab74fe92
blackdeath <blackdeath166@gmail.com>
2020-01-26 19:11:01 -0600

    Trabajando en doc Interna

--- src/operaciones/mutations/docInterna.mutation.ts
+++ src/operaciones/mutations/docInterna.mutation.ts
@@ -8,7 +8,6 @@ export async function agDocInterna(agNotificacion: any, db: any)
         agNotificacion.num = totalNotificaciones;
         let anoActual = new Date().getFullYear();
         agNotificacion.folioInterno = `FOL-${agNotificacion.num}-SIMAPAS/${anoActual}`;
-        console.log('Doc interna', agNotificacion);
         return await db.collection("docInterna").insertOne(agNotificacion).then(
             async () =>
             {
@@ -37,7 +36,6 @@ export async function acVistoPorUsuario(usuario: string, folioInterno: string, d
         false, true).then(
         async (res: any) =>
         {
-            console.log('Notificacion vista', res);
             return {
                 estatus: true,
                 mensaje: 'La notificacion ha modificado como vista',
--- src/operaciones/querys/docInterna.ts
+++ src/operaciones/querys/docInterna.ts
@@ -3,7 +3,8 @@ export async function todasNotificacionesDocInterna(db: any) {
 }
 
 export async function todasNotificacionesUsuario(usuario: string, db: any) {
-    return await db.collection("docInterna").find({"usuarioDestino.usuario": usuario}).toArray();
+    // return await db.collection("docInterna").find({"usuarioDestino.usuario": usuario}).toArray();
+    return await db.collection("docInterna").find({"usuarioDestino": {$elemMatch: {usuario, visto: true}}}).toArray();
 }
 
 export async function docInternaUsuarioVisto(usuario: string, visto: boolean, db: any) {
