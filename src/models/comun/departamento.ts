06ebc35c49b9d649e9eb15f3aa391d501e0aa4f4
blackdeath <blackdeath166@gmail.com>
2020-06-09 06:30:04 -0500

    Se quito mongoose y se cambio la version de express para que pudira crear el build

--- src/models/comun/departamento.ts
+++
@@ -1,8 +0,0 @@
-import {model, Schema} from 'mongoose';
-
-
-let departamentoSchema = new Schema({
-    nombre: {type: String, unique: true, required: [true, 'El departamento es un campo requerido']}
-});
-
-export default model('Departamento', departamentoSchema);
--- src/models/subirArchivos.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const archivosSubirSchema = new mongoose.Schema({
-    _id: {type: String, required: 'El id es requerido'},
-    archivoUrl: {type: String}
-});
-module.exports = mongoose.model('Archivos', archivosSubirSchema);
