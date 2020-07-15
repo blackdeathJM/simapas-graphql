3b1e51c844354451c4877d0399d3a2ef30d88b7d
BLACKDEATH <blackdeath166@gmail.com>
2020-07-14 21:00:16 -0500

    Se actualizaron las dependencias al dia 14/07/2020, funcionando de nuevo las importaciones de express y quite el import de express core, verificar las versiones de express como la de los types para cuando se actualizen las dependencias a versiones nuevas y por el momento usamos la version de graphql a la 14.0.6

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import graphqlHTTP from 'express-graphql'
+import graphqlHTTP from 'express-graphql';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 import {ApolloServer, PubSub} from "apollo-server-express";
