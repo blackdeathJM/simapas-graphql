c26cf12a28ce2b21ab7632dc729f7bd6427599cb
blackdeath <blackdeath166@gmail.com>
2020-07-16 18:35:35 -0500

    Se modularizaron los schemas nueva forma 2020

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,10 +1,12 @@
-import 'graphql-import-node';
+import path from 'path';
 import {GraphQLSchema} from "graphql";
-import resolvers from './../resolvers/resolversMap';
 import {makeExecutableSchema} from "graphql-tools";
-import {fileLoader, mergeTypes} from 'merge-graphql-schemas';
+import resolvers from './../resolvers/resolversMap';
+import {loadFilesSync} from "@graphql-tools/load-files";
+import {mergeTypeDefs} from "@graphql-tools/merge";
 
-const typeDefs = mergeTypes(fileLoader(`${__dirname}/**/*.graphql`), {all: true});
+const typesArray = loadFilesSync(path.join(__dirname, './**/*.graphql'));
+const typeDefs = mergeTypeDefs(typesArray);
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
