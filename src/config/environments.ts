5e51d105f1c6b23f97ea6dac7b5572fd5cdf0c13
BLACKDEATH <blackdeath166@gmail.com>
2020-08-24 05:05:48 -0500

    Se creo de nuevo la documentacion

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 
 const environments = dotenv.config({path: './src/.env'});
 
-if (process.env.NODE_ENV !== 'production') {
-    if (environments.error) {
+if (process.env.NODE_ENV !== 'production')
+{
+    if (environments.error)
+    {
         throw  environments.error;
     }
 }
