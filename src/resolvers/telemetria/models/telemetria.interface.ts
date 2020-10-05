96ea94eeb1668630dda2c6d4bb83b637071d51c9
BLACKDEATH <blackdeath166@gmail.com>
2020-10-04 22:22:05 -0500

    Se quito schema de docInterna, y se implemetaron las consultas entre fechas y por coicidencia

--- src/resolvers/telemetria/models/telemetria.interface.ts
+++ src/resolvers/telemetria/models/telemetria.interface.ts
@@ -5,13 +5,63 @@ export interface IInstalacion
     direccion: string;
     noInstalacion: number;
     activo: boolean;
-    telemetria: Array<ITelemetria>;
+    telemetria: ITelemetria;
+    parametrosElectricos: IParametrosElectricos;
+    bomba: IBomba[]
 }
 
 export interface ITelemetria
 {
-    radio: Array<string>;
-    plc: Array<string>;
-    switch: Array<string>;
-    repetidor: Array<string>;
+    radio: string[];
+    plc: string[];
+    switch: string[];
+    repetidor: string[];
+}
+
+export interface IParametrosElectricos
+{
+    ano: string;
+    mes: string;
+    fechaRegistro: string;
+    voltajes: IVoltajes[];
+    amperajes: IAmperajes[];
+    factorPotencia: IFactorPotencia[];
+}
+
+interface IVoltajes
+{
+    v1: number;
+    v2: number;
+    v3: number;
+    totalV: number;
+}
+
+interface IAmperajes
+{
+    a1: number;
+    a2: number;
+    a3: number;
+    totalA: number;
+}
+
+interface IFactorPotencia
+{
+    f1: number;
+    f2: number;
+    f3: number;
+    totalF: number;
+}
+
+interface IBomba
+{
+    descripcion: string;
+    marca: string;
+    serie: string;
+    hp: number;
+    eficiencia: number;
+    fechaInstalacion: string;
+    FechaRetiro: string;
+    observaciones: string;
+    imgEvidencia: string[];
+    activa: boolean;
 }
