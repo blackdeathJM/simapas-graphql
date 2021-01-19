0911cc5c786c9189662c712523533fe4f73df291
Jose Omar <blackdeath166@gmail.com>
2021-01-19 05:19:29 -0600

    trabajando en folios

--- src/server.ts
+++ src/server.ts
@@ -26,7 +26,8 @@ async function init()
 
     app.use(function (req, res, next)
     {
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        // res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.setHeader("Access-Control-Allow-Origin", "http://192.168.1.189:5642");
         res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS,');
         res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
