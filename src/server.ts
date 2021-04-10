031ce3480407399093ca8357b27030bbe458cf41
omar <blackdeath166@gmail.com>
2021-04-10 14:25:48 -0500

    recibos de cfe

--- src/server.ts
+++ src/server.ts
@@ -30,9 +30,8 @@ async function init()
     app.use(function (req, res, next)
     {
         res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        // res.setHeader("Access-Control-Allow-Origin", "http://26.62.148.73:5642");
-        // res.setHeader("Access-Control-Allow-Origin", "https://26.84.127.13:5642");
-        // res.setHeader("Access-Control-Allow-Origin", "http://192.168.1.189:5642");
+        // res.setHeader("Access-Control-Allow-Origin", "https://26.62.148.73:5642");
+        // res.setHeader("Access-Control-Allow-Origin", "https://192.168.1.189:5642");
         res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS,');
         res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
