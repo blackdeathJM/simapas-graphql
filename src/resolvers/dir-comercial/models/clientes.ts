702fb7d84f165b2d9aa7ae18980ed82adef9bffe
omar <blackdeath166@gmail.com>
2021-04-19 20:12:36 -0500

    Orden trabajo telemetria

---
+++ src/resolvers/dir-comercial/models/clientes.ts
@@ -0,0 +1,56 @@
+export interface IClientes
+{
+
+    folio: number;
+    codigoQr: string;
+    rpu: string;
+    noCta: number;
+    nombre: string;
+    apellidos: string;
+    telefonos: [String]
+    email: string;
+    rfc: string;
+    iva: number;
+    estatusUsuario: string;
+    colorEstatusUsuario: string;
+    grupoFact: number;
+    deposito: number;
+    envio: string;
+    prorroga: string;
+    hyperlink: string;
+    activoEnInternet: boolean;
+    usuarioActivo: boolean;
+    obs: string;
+    contratos: IContrato[];
+}
+
+export interface IContrato
+{
+    noMedidor: string;
+    noContrato: string;
+    fechaAlta: string;
+    calle: string;
+    entreCalles: string;
+    referencia: string;
+    colonia: string;
+    codigoPostal: string;
+    ciudad: string;
+    municipio: string;
+    pais: string;
+    estado: string;
+    sector: number;
+    ruta: number;
+    tarifa: string;
+    giro: string;
+    zona: string;
+    noFamiliasEnToma: number;
+    documentacion: IDocumentacion
+    activo: Boolean
+}
+
+export interface IDocumentacion
+{
+    identificacion: string[];
+    predial: string[];
+    curp: string;
+}
