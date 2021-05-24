5d390a8fd0b0bb5adde055874d0e18f01bb07c32
omar <blackdeath166@gmail.com>
2021-05-23 20:01:24 -0500

    informacion general de clientes

--- src/resolvers/dir-comercial/models/cliente.interface.ts
+++ src/resolvers/dir-comercial/models/cliente.interface.ts
@@ -49,6 +49,7 @@ export interface ISolicitudServ
     noCuentaRef: string;
     tipoPredio: string;
     areaPredio: number;
+    areaConstruida: number;
     almacenamiento: string;
     tipoDeUso: string;
     MaterialArroyoDeCalle: string;
