f423b82d55907ff386929fd8fba9e3034cd09b57
Jose Omar <blackdeath166@gmail.com>
2022-02-27 16:47:51 -0600

    Actualizacion de dependencias

--- src/configMuter/docs.routes.ts
+++ src/configMuter/docs.routes.ts
@@ -1,4 +1,4 @@
 import express from 'express';
-import {agDocs, obDocs} from './docs';
+import {obDocs} from './docs';
 
-export const router = express.Router().post('/docs', agDocs).get('/:archivoUrl', obDocs);
+export const router = express.Router().get('/:archivoUrl', obDocs);
